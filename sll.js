dd16f32cfabba371ad881ad29b033e01335ac074
saimaja <saimaakhtar@gmail.com>
2020-10-06 22:44:43 -0400

    pop shift unshift solutions

--- sll.js
+++ sll.js
@@ -20,12 +20,49 @@ class SLL{
             this.tail.next = newNode;
             this.tail = newNode;
         }
-        this.length++
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length --;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    shift(){
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length --;
+        return currentHead;
+    }
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+        newNode.next = this.head;
+        this.head = newNode;
+        }
+        this.length++;
         return this;
     }
 }
 
 let list = new SLL()
 list.push('hi')
-list.push('bye')
+list.push('!')
+list.unshift('hey')
 console.log(list)
