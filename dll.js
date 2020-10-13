667e4a1ba853b055bd3951f70a5ff993b72079af
saimaja <saimaakhtar@gmail.com>
2020-10-13 12:12:40 -0400

    solution for push and pop

--- dll.js
+++ dll.js
@@ -12,4 +12,39 @@ class DLL {
         this.tail = null;
         this.length = 0;
     }
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let poppedTail = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedTail.prev;
+            this.tail.next = null;
+            poppedTail.prev = null;
+        }
+        this.length--
+        return poppedTail;
+    }
 }
+
+let dll = new DLL();
+dll.push(13)
+dll.push(14)
+dll.push(15)
+dll.push(16)
+dll.pop()
+console.log(dll)
